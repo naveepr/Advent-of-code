3cbe87a6d8157f0f8a5be9776d43f11b4e17e449
Naveen <naveepr@gmail.com>
2020-01-01 19:39:15 -0800

    Added the solution code

--- 2015/1/main.cpp
+++ 2015/1/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(int argc, const char *argv[])
 {
+    string fileName;
+    char c;
+    long int count;
+
+    cout<<"Enter the name of the file"<<endl;
+    cin >> fileName;
     
+    ifstream is(fileName);
+
+    while(is.get(c)) {
+        switch(c) {
+            case '(':
+            case '{':
+            case '[':
+                        count++;
+                        break;
+            case ')':
+            case '}':
+            case ']':
+                        count--;
+                        break;
+            default:
+                        break;
+        }
+    }
+
+    is.close();
+    cout<<count<<endl;
     return 0;
 }
